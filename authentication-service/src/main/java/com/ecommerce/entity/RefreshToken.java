883129f971aa8249564d3812c3aecf4cad610aa9
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-29 18:01:54 -0300

    refactor: extend RefreshToken from TokenEntity and remove redundant fields

--- authentication-service/src/main/java/com/ecommerce/entity/RefreshToken.java
+++ authentication-service/src/main/java/com/ecommerce/entity/RefreshToken.java
@@ -1,20 +1,11 @@
 package com.ecommerce.entity;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-import java.time.LocalDateTime;
-
 @Entity
 @Table(name = "refresh_tokens")
-public class RefreshToken extends PanacheEntity {
+public class RefreshToken extends TokenEntity {
 
-    @Column(unique = true)
-    public String hashedToken;
-    public String encryptedToken;
-    public Long userId;
-    public LocalDateTime expiresAt;
     public boolean revoked = false;
 }
