5b4e0e630a509984333ff233d765e1dfde24c458
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-29 18:02:39 -0300

    feat: add EmailProducer for emitting authentication events to Kafka

---
+++ authentication-service/src/main/java/com/ecommerce/producer/EmailProducer.java
@@ -0,0 +1,30 @@
+package com.ecommerce.producer;
+
+import com.ecommerce.event.TokenUrlEvent;
+import com.ecommerce.event.TokenConfirmationEvent;
+import org.eclipse.microprofile.reactive.messaging.Channel;
+import org.eclipse.microprofile.reactive.messaging.Emitter;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.event.Observes;
+import jakarta.inject.Inject;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+public class EmailProducer {
+
+    private static final Logger LOG = Logger.getLogger(EmailProducer.class);
+
+    @Inject
+    @Channel("authentication-email")
+    Emitter<Object> emailEmitter;
+
+    public void onTokenUrl(@Observes TokenUrlEvent event) {
+        LOG.infof("Emitting URL event to Kafka for %s", event.email());
+        emailEmitter.send(event);
+    }
+
+    public void onTokenConfirmation(@Observes TokenConfirmationEvent event) {
+        LOG.infof("Emitting confirmation event to Kafka for %s", event.email());
+        emailEmitter.send(event);
+    }
+}
