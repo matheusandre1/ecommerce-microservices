ae8ad87f37c3f27e95f29f348ef0b1efa9c4889b
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-29 18:03:32 -0300

    feat: add TokenEntity abstract class for common token entity fields

---
+++ authentication-service/src/main/java/com/ecommerce/entity/TokenEntity.java
@@ -0,0 +1,17 @@
+package com.ecommerce.entity;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public abstract class TokenEntity extends PanacheEntity {
+
+    @Column(unique = true)
+    public String hashedToken;
+    public String encryptedToken;
+    public Long userId;
+    public LocalDateTime expiresAt;
+}
