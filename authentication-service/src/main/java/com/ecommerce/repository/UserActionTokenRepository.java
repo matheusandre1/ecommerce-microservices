e20b3f1fd3f5ce3805142808fe4b7001263e7d9f
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-29 18:03:16 -0300

    feat: add UserActionTokenRepository for user action token persistence

---
+++ authentication-service/src/main/java/com/ecommerce/repository/UserActionTokenRepository.java
@@ -0,0 +1,8 @@
+package com.ecommerce.repository;
+
+import com.ecommerce.entity.UserActionToken;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
+public class UserActionTokenRepository extends TokenRepository<UserActionToken> {
+}
