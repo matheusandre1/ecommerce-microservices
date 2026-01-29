131349acf06d39798f6a147db4c7bc284f0ef67b
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-29 18:02:44 -0300

    feat: add PasswordResetRequest DTO for password reset email submission

---
+++ authentication-service/src/main/java/com/ecommerce/dto/PasswordResetRequest.java
@@ -0,0 +1,6 @@
+package com.ecommerce.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record PasswordResetRequest(@NotBlank @Email String email) {}
