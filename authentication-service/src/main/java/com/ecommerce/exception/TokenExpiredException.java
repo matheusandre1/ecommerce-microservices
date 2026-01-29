525ba0958e0fb29a240913e09eefaf141d657354
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-29 18:03:24 -0300

    feat: add TokenExpiredException for handling expired token scenarios

---
+++ authentication-service/src/main/java/com/ecommerce/exception/TokenExpiredException.java
@@ -0,0 +1,8 @@
+package com.ecommerce.exception;
+
+public class TokenExpiredException extends RuntimeException {
+
+    public TokenExpiredException(String message) {
+        super(message);
+    }
+}
