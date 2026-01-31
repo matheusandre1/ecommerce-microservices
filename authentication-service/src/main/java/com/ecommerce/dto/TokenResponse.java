0f5ba88be895b5d40dd95de55c0d74ed69541629
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-31 17:03:22 -0300

    chore: add @RegisterForReflection to TokenResponse record

--- authentication-service/src/main/java/com/ecommerce/dto/TokenResponse.java
+++ authentication-service/src/main/java/com/ecommerce/dto/TokenResponse.java
@@ -1,3 +1,6 @@
 package com.ecommerce.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
+
+@RegisterForReflection
 public record TokenResponse(String accessToken, String refreshToken) {}
