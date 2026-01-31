87ea51ec799280cf4bf73c97eecb97d0c7bd7404
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-31 17:01:19 -0300

    chore: add @RegisterForReflection to ActivationRequest record

--- authentication-service/src/main/java/com/ecommerce/dto/ActivationRequest.java
+++ authentication-service/src/main/java/com/ecommerce/dto/ActivationRequest.java
@@ -1,5 +1,7 @@
 package com.ecommerce.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import jakarta.validation.constraints.NotBlank;
 
+@RegisterForReflection
 public record ActivationRequest(@NotBlank String token) {}
