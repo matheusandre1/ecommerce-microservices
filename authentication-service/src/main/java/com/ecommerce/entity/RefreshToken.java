ad04634288f9600da54fa9f7dde7407f6d8b1923
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-31 17:03:03 -0300

    chore: add SequenceGenerator annotation to RefreshToken entity

--- authentication-service/src/main/java/com/ecommerce/entity/RefreshToken.java
+++ authentication-service/src/main/java/com/ecommerce/entity/RefreshToken.java
@@ -1,10 +1,16 @@
 package com.ecommerce.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "refresh_tokens")
+@SequenceGenerator(
+        name = "refresh_tokens_seq_gen",
+        sequenceName = "refresh_tokens_seq",
+        allocationSize = 50
+)
 public class RefreshToken extends TokenEntity {
 
     public boolean revoked = false;
