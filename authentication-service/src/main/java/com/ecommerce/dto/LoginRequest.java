2d8f2a6a59d942152fe50799d567310b4ff57f5a
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-31 17:02:20 -0300

    chore: add @RegisterForReflection to LoginRequest record

--- authentication-service/src/main/java/com/ecommerce/dto/LoginRequest.java
+++ authentication-service/src/main/java/com/ecommerce/dto/LoginRequest.java
@@ -1,8 +1,10 @@
 package com.ecommerce.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+@RegisterForReflection
 public record LoginRequest(
         @NotBlank @Email String email,
         @NotBlank String password
