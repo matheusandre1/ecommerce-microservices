97a1efa6a9098d99d801cab93eb26fecaddfda1f
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-31 17:02:24 -0300

    chore: add @RegisterForReflection to PasswordResetRequest record

--- authentication-service/src/main/java/com/ecommerce/dto/PasswordResetRequest.java
+++ authentication-service/src/main/java/com/ecommerce/dto/PasswordResetRequest.java
@@ -1,6 +1,8 @@
 package com.ecommerce.dto;
 
+import io.quarkus.runtime.annotations.RegisterForReflection;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
+@RegisterForReflection
 public record PasswordResetRequest(@NotBlank @Email String email) {}
