40fe4f3f266d758a3cae50668db771afb8410cc2
Matheus Oliveira <matheus.6148@gmail.com>
2026-01-31 17:03:17 -0300

    chore: add @RegisterForReflection to TokenConfirmationEvent record

--- authentication-service/src/main/java/com/ecommerce/event/TokenConfirmationEvent.java
+++ authentication-service/src/main/java/com/ecommerce/event/TokenConfirmationEvent.java
@@ -1,5 +1,7 @@
 package com.ecommerce.event;
 
 import com.ecommerce.entity.ActionType;
+import io.quarkus.runtime.annotations.RegisterForReflection;
 
+@RegisterForReflection
 public record TokenConfirmationEvent(Long userId, String email, ActionType actionType) {}
