cf38c15e644bde4a12c25094b86c27e2852b2f08
Matheus Oliveira <matheus.6148@gmail.com>
2026-02-07 20:10:01 -0300

    test: remove unused import in ProductResourceTest

--- product-service/src/test/java/com/ecommerce/resource/ProductResourceTest.java
+++ product-service/src/test/java/com/ecommerce/resource/ProductResourceTest.java
@@ -5,7 +5,6 @@ import io.quarkus.test.common.http.TestHTTPEndpoint;
 import io.quarkus.test.junit.QuarkusTest;
 import io.quarkus.test.security.TestSecurity;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigDecimal;
